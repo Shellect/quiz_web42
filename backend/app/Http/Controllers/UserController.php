086177f4af0c4b209d6eddb7e6aa6f62d0fc0d9f
Your name <example@example.com>
2025-11-19 21:23:46 +0300

    add user resource

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use \Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -12,5 +14,31 @@ class UserController extends Controller
     {
       
         return response()->json(User::all());
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|unique:user',
+            'email' => 'email|required|unique:user',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $validated = $validator->validated();
+        $hashedPassword = Hash::make($validated['password']);
+
+        $user = User::create([
+            'username' => $validated['username'],
+            'email' => $validated['email'],
+            'password_hash' => $hashedPassword
+            
+        ]);
+        return response()->json($user, 201);
     }
 }
